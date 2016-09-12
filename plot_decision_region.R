f935a1f419e503f06273c536c36c5b9655cf09db
espedesuza <espedesuza@gmail.com>
2016-09-12 11:59:23 +0200

    decision plot more robust

--- plot_decision_region.R
+++ plot_decision_region.R
@@ -1,24 +1,55 @@
 #plot 2D decision boundary
-plot_decision_region <- function(x,y,model,nb_points_by_axe) {
-        nb_points_by_axe = 100
-        x1d_min = trunc(min(x[,1]))-1
-        x1d_max = trunc(max(x[,1]))+1
-        x2d_min = trunc(min(x[,2]))-1
-        x2d_max = trunc(max(x[,2]))+1
-        x1_seq=seq(from=x1d_min, to=x1d_max, length.out = nb_points_by_axe)
-        x2_seq=seq(from=x2d_min, to=x2d_max, length.out = nb_points_by_axe)
+# plot_decision_region <- function(x,y,model,nb_points_by_axe) {
+plot_decision_region <- function(data,nb_points_by_axe, model, ...) {
+        # nb_points_by_axe = 100
         
-        x_grid = expand.grid(x1=x1_seq
-                             ,x2=x2_seq
-        )
-        mod15 <- predict(model, newdata=x_grid, type="prob")
-        prob15 <- matrix(mod15, nb_points_by_axe, nb_points_by_axe)
+        # if ( is.integer(outcome_var) | is.numeric(outcome_var) ) 
+        #         outcome_var<-colnames(data)[outcome_var]
+        
+        # equation = as.character(attr(model,"predvars"))
+        
+        predictors = model$final$xNames
+        predictors = intersect(colnames(data),predictors) #keep simple vars
+        
+        df <- NULL
+        df <- data.frame(index=1:nb_points_by_axe) #initialisation en nb de rows
+        for (var in predictors) {
+                df_tmp <- NULL
+                # creer une rangée de valeurs possibles pour chaque predictor
+                # assign(paste0(var,'_min'), trunc(min(data[,var]))-1)
+                # assign(paste0(var,'_max'), trunc(max(data[,var]))+1)
+                assign(paste0(var,'_seq'), seq(
+                        from=trunc(min(data[,var]))-1
+                        ,to=trunc(max(data[,var]))+1
+                        ,length.out = nb_points_by_axe
+                        ))
+                df_tmp <- data.frame(get(paste0(var,'_seq')))
+                colnames(df_tmp)[1]<- var
+                df <- cbind(df,df_tmp)
+        }
+        df <- df[,-1]
+        # x1d_min = trunc(min(x[,1]))-1
+        # x1d_max = trunc(max(x[,1]))+1
+        # x2d_min = trunc(min(x[,2]))-1
+        # x2d_max = trunc(max(x[,2]))+1
+        # x1_seq=seq(from=x1d_min, to=x1d_max, length.out = nb_points_by_axe)
+        # x2_seq=seq(from=x2d_min, to=x2d_max, length.out = nb_points_by_axe)
+        
+        x_grid = expand.grid(df)
+        
+        preds <- predict(model, newdata=x_grid
+                         , type="prob"
+                         )
+        probs <- matrix(preds[,1], nb_points_by_axe, nb_points_by_axe)
         par(mar=rep(2,4),pty="s")
-        plot(x, type = "n")  # setting up coord. system
-        # plot(-5:5,-5:5, type = "n")
-        contour(x1_seq, x2_seq, prob15, levels=0.5, labels="", xlab="", ylab=""
+        plot(data[,1],data[,2], type = "n")  # setting up coord. system
+        # contour(x1_seq, x2_seq, probs, levels=0.5, labels="", xlab="", ylab=""
+        #         , axes=FALSE)
+        x1_seq = unique(x_grid[,1])
+        x2_seq = unique(x_grid[,2])
+        contour(x1_seq, x2_seq, probs, levels=0.5, labels="", xlab="", ylab=""
                 , axes=FALSE)
-        points(x, col=ifelse(y==1, "coral", "cornflowerblue"))
-        points(x_grid, pch=".", cex=1, col=ifelse(prob15<0.5, "coral", "cornflowerblue"))
+        points(data[,1],data[,2], col=ifelse(y==1, "coral", "cornflowerblue"))
+        points(x_grid, pch=".", cex=1, col=ifelse(probs<0.5, "coral", "cornflowerblue"))
         box()
 }
